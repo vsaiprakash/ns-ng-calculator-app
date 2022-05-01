6f4c1361150b7a8987d02a96dcf1f4dd916d1f9c
V. Sai Prakash <vukkumsp@gmail.com>
2022-05-01 18:18:38 +0530

    refactoring code for a calc.

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core'
 
-import { Item } from './item'
-import { ItemService } from './item.service'
-
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-  items: Array<Item>
+  cellColor: string = "#d4d4d4";
+  resultColor: string = "#f0f0f0";
 
-  constructor(private itemService: ItemService) {}
+  customMargin: string = "2";
+  customBorderRadius: string = "30px";
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
   }
 }
