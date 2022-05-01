a2a8fdcfcc76f99e68c22be5e18fe6adbd98fd65
V. Sai Prakash <vukkumsp@gmail.com>
2022-05-01 18:54:12 +0530

    basic ui addedfor calculator.

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Button, EventData } from '@nativescript/core';
 
 @Component({
   selector: 'ns-items',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-  cellColor: string = "#d4d4d4";
+
+  cellColor1: string = "#e8e8e8";
+  cellColor2: string = "#c7c7c7";
+  cellColor3: string = "#ababab";
+  cellColor4: string = "#949494";
+
   resultColor: string = "#f0f0f0";
+  clearColor: string = "#ff7057";
+  equalBtnColor: string = "#5797ff";
 
   customMargin: string = "2";
   customBorderRadius: string = "30px";
@@ -16,4 +24,10 @@ export class ItemsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  onTap(args: EventData) {
+    let button = args.object as Button;
+    // execute your custom logic here...
+    console.log(args.eventName);
+}
 }
